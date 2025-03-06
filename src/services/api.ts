9745c217d1072d80aa95a4d3967ca4c67cdd9e34
dnakatate@tijob.tech <nakalissi@gmail.com>
2025-03-06 20:46:00 -0300

    feat: vercel rewrites

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,15 +2,11 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  withCredentials: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  }, 
 });
 
 export const fetchRestaurantDetails = async (restaurantId: string) => {
   try {
-    const response = await api.get(`/challenge/venue/${restaurantId}`);
+    const response = await api.get(`/challenge/venue/${restaurantId}`, {});
     return response.data;
   } catch (error: any) {
     throw new Error(error);
